9b14fd380650aec198fdee9364baaefe81f17229
Rodrigo Soares <meuemail.rodrigodev@gmail.com>
2022-09-22 22:26:48 -0300

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './components/Home'
+import Modal from './components/Modal'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <>
+      <Home/>
+      <Modal/>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
